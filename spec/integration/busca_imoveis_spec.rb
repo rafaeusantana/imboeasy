67a5c641a8ba4e3b9b0f17f7c874c57ca0893901
Rafael Santana <rafa.eu.santana@gmail.com>
2015-01-09 18:38:41 -0200

    Mais um teste

--- spec/integration/busca_imoveis_spec.rb
+++ spec/integration/busca_imoveis_spec.rb
@@ -8,7 +8,9 @@ RSpec.describe "busca de imóveis", :type => :request do
       assert_select "input[name=?]", "imovel[cidade]"
       assert_select "select[name=?]", "imovel[estado_id]"
     end
+  end
 
+  it "responde à busca corretamente" do
     Imovel.create!(cidade: "São Paulo", endereco: "Av. Paulista, 300", descricao: "Um lindo apartamento!", qtd_quartos: 3, metragem: 60, vagas_auto: 1, valor_aluguel: 2000, valor_condominio: 1200, tipo_imovel_id: 1, estado_id: 1, qtd_suites: 1, ano_construcao: 1980, bairro: "Jardim Paulista")
 
     get "/imovels", :bairro => "Jardim Paulista", :cidade => "São Paulo"
---
+++ spec/models/imoveis_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe Imovel, :type => :model do
+  it "cadastro de imóveis" do
+    imovel = Imovel.create!(cidade: "São Paulo", endereco: "Av. Paulista, 300", descricao: "Um lindo apartamento!", qtd_quartos: 3, metragem: 60, vagas_auto: 1, valor_venda: 600000, valor_aluguel: 2000, valor_condominio: 1200, tipo_imovel_id: 1, estado_id: 1, qtd_suites: 1, ano_construcao: 1980, bairro: "Jardim Paulista")
+
+    expect(Imovel.where(:endereco => "Av. Paulista, 300")[0]).to eq(imovel)
+  end
+
+  it "busca de imóveis" do
+    imovel = Imovel.create!(cidade: "São Paulo", endereco: "Av. Paulista, 300", descricao: "Um lindo apartamento!", qtd_quartos: 3, metragem: 60, vagas_auto: 1, valor_aluguel: 2000, valor_condominio: 1200, tipo_imovel_id: 1, estado_id: 1, qtd_suites: 1, ano_construcao: 1980, bairro: "Jardim Paulista")
+
+    imovels = Imovel.selecionarImoveis({cidade: "São Paulo", bairro: "Jardim Paulista"})
+
+    expect(imovels[0]).to eq(imovel)
+  end
+end
